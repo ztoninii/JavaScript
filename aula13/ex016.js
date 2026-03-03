0e540bef430b0cd8581b63d00291f9b3d8a50576
Yago Tonini <yagotonini2@gmail.com>
2026-03-03 18:13:25 -0300

    aula 13

---
+++ aula13/ex016.js
@@ -0,0 +1,22 @@
+
+var controla = 1
+do {
+    console.log(`Passo ${controla}`)
+    controla++
+} while (controla <= 10)
+console.log('FIM!')
+
+
+
+
+
+
+
+/*
+var controla = 1
+while (controla <= 10) {
+    console.log(`Passo ${controla}`)
+    controla++
+} 
+*/
+

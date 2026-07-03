a08e3e757f3abff871f59b01ab9c3bc9f27e982d
Yago Tonini <yagotonini2@gmail.com>
2026-07-03 15:24:28 -0300

    Aulas 13 e 14
    Aprendendo sobre repetição em JS.
    Depuração de Código (Debug) muito útil.

--- aula12/ex011.js
+++ aula12/ex011.js
@@ -1,11 +1,11 @@
-var idade = 100
+var idade = 10
 console.log(`Você tem ${idade} anos.`)
 
 if (idade < 16) {
-    console.log('Não vota')
+    console.log('Não vota.')
 }  else if(idade < 18 || idade > 65) {
-    console.log('Voto opcional')
+    console.log('Voto opcional.')
 }  else{
-    console.log('Voto obrigatório')
+    console.log('Voto obrigatório.')
 }
   
--- aula13/ex016.js
+++ aula13/ex016.js
@@ -13,10 +13,10 @@ console.log('FIM!')
 
 
 /*
-var controla = 1
-while (controla <= 10) {
-    console.log(`Passo ${controla}`)
-    controla++
+var c = 1
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++
 } 
 */
 
